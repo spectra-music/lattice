f1d51de836223659bfd290d6726a9d5e0dee5794
Katherine Whitlock <toroidalcode@gmail.com>
2014-07-14 17:45:56 -0400

    More MDB stuff

---
+++ include/type.h
@@ -0,0 +1,35 @@
+#ifndef LTC_TYPE_H
+#define LTC_TYPE_H
+#include <stdlib.h>
+#include "vertex.h"
+
+
+#ifndef LTC_MAX_TYPE_NAME_LEN
+#define LTC_MAX_TYPE_NAME_LEN 30
+#endif // LTC_MAX_TYPE_NAME_LEN
+
+#ifdef __cplusplus 
+extern "C" {
+#endif
+  
+typedef struct ltc_vertex_type_t {
+  char name[LTC_MAX_TYPE_NAME_LEN];
+  struct ltc_vertex_type_t **parents;    // This is a list of direct parents only
+  size_t num_parents;
+  char **fields;
+} ltc_vertex_type_t;
+
+
+extern ltc_vertex_type_t *ltc_create_vertex_type(const char *name);
+extern ltc_vertex_type_t *ltc_create_vertex_type_with_parents(const char *name, const char **parents);
+extern ltc_vertex_type_t *ltc_vertex_type_get(const char *name);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* LTC_TYPE_H */
+
+
+
+
--- lib/lattice.c
+++ lib/lattice.c
@@ -12,22 +12,52 @@
  */
 int main(int argc, char* argv[]) {
   if (argc == 1) {
-    printf("Usage: lattice_demo env/my_db.ltc");
+    printf("Usage: lattice_demo env");
   }
 
-  char dest[sizeof(argv[1])];
+  char dest[_POSIX_PATH_MAX];
   get_full_path_nonexistant(dest, argv[1]);
   MDB_env *env;  /* This is the database environment (directory, etc.) */
 
-  /* Try to initialize the environment */
+  /* Try to create the environment */
   int rv = mdb_env_create(&env);
-  if ( rv != 0 ) {
-    printf("Error creating database environment. Error code: %d\n Exiting now.\n", rv);Xb
+  if (rv != 0) {
+    printf("Error creating database environment.\nError code: %d\n Exiting now.\n", rv);
+    mdb_env_close(env);
     exit(EXIT_FAILURE);
   }
-  
-  MDB_dbi dbi;
 
+  /* Try to open the environment */
+  rv = mdb_env_open(env, dest, 0, 755);
+  if (rv != 0) {
+    printf("Error opening database environment.\nError code: %d\n Exiting now.\n", rv);
+    mdb_env_close(env);
+    exit(EXIT_FAILURE);
+  }
+
+  MDB_dbi dbi;
+  MDB_txn *txn;
+  rv = mdb_txn_begin(env, NULL, 0, txn);
+  if (rv != 0) {
+    printf("Error opening database: Could not create transaction.\n"
+           "Error code: %d\n Exiting now.\n", rv);
+    mdb_env_close(env);
+    exit(EXIT_FAILURE);
+  }
+
+  rv = mdb_dbi_open(txn, NULL, 0, dbi);
+  if (rv != 0) {
+    printf("Error opening database.\n"
+           "Error code: %d\n Exiting now.\n", rv);
+    mdb_txn_abort(txn);
+    mdb_env_close(env);
+    exit(EXIT_FAILURE);
+  }
+
+
+
+  mdb_dbi_close(env,dbi);
+  mdb_env_close(env);
   /* lattice *lat = lattice_create(); */
   return 0;
 }
