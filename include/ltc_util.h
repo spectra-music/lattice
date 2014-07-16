1a97dd0b3db4f1e8573f9ac7a3dff8413e6c82e4
Zachary Elliott <zach@nyu.edu>
2014-07-15 23:15:25 -0400

    Cleaning ltc_util header file

--- include/ltc_util.h
+++ include/ltc_util.h
@@ -1,14 +1,20 @@
-#ifndef LTC_UTIL_H
-#define LTC_UTIL_H
+#ifndef __LTC_UTIL_H__
+#define __LTC_UTIL_H__ 1
+
 
 #ifdef __cplusplus
 extern "C" {
 #endif
+#if 0
+}
+#endif
+
 
 extern void get_full_path_nonexistant(char *dest, const char *relative_path);
+
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif /* LTC_UTIL_H */
+#endif /* __LTC_UTIL_H__ */
