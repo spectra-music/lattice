64ef9384a4b53ff49ac1ff2518b9c4788304987b
Zachary Elliott <zach@nyu.edu>
2014-07-15 23:26:12 -0400

    Cleaning lattice header file

--- include/lattice.h
+++ include/lattice.h
@@ -1,13 +1,22 @@
-#ifndef LATTICE_H
-#define LATTICE_H
-#ifdef __cplusplus 
+#ifndef __LATTICE_H__
+#define __LATTICE_H__ 1
+
+
+#ifdef __cplusplus
 extern "C" {
 #endif
-  
-  extern void prepend_string(char *string, const char* prefix);
-  extern char *get_full_path_nonexistant(const char *relative_path);
-  
+#if 0
+}
+#endif
+
+
+extern void prepend_string(char *string, const char *prefix);
+
+extern char *get_full_path_nonexistant(const char *relative_path);
+
+
 #ifdef __cplusplus
 }
 #endif
-#endif /* LATTICE_H */
+
+#endif /* __LATTICE_H__ */
