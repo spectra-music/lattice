63f65c0d82f05f62861157027a86aab4d1de2dd7
Zachary Elliott <zach@nyu.edu>
2014-07-15 23:16:45 -0400

    Cleaning ltc_vertex header file

--- include/ltc_vertex.h
+++ include/ltc_vertex.h
@@ -1,13 +1,29 @@
-#ifndef LTC_VERTEX_H
-#define LTC_VERTEX_H
+#ifndef __LTC_VERTEX_H__
+#define __LTC_VERTEX_H__ 1
+
+
 #include "type.h"
 
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+#if 0
+}
+#endif
+
+
 /**
- * @brief The in-memory 
+ * @brief The in-memory
  */
-typedef struct {
+typedef struct ltc_vertex ltc_vertex_t;
+struct ltc_vertex {
   char **type; // e.g. ["Color", "RGBColor"]
 } ltc_vertex_t;
 
 
-#endif /* LTC_VERTEX_H */
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __LTC_VERTEX_H__ */
